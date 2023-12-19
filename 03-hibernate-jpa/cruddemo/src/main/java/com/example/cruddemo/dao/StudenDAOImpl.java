6abcdfe18bc60a46cc9ff8855fdca09b5091eee8
Angel Motta <angel.motta@utec.edu.pe>
2023-12-19 16:56:08 -0500

    feat: dao implementation

---
+++ 03-hibernate-jpa/cruddemo/src/main/java/com/example/cruddemo/dao/StudenDAOImpl.java
@@ -0,0 +1,25 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudenDAOImpl implements StudentDAO {
+
+    private EntityManager entityManager;
+
+    // Constructor injection
+    @Autowired
+    public StudenDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
