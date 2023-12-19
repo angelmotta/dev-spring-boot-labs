d9de5a61ae6859b31509832e8ea4d162c947d1a3
Angel Motta <angel.motta@utec.edu.pe>
2023-12-19 18:05:38 -0500

    feat: persist method

--- 03-hibernate-jpa/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-hibernate-jpa/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.cruddemo;
 
+import com.example.cruddemo.dao.StudentDAO;
+import com.example.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,15 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello world");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		Student s = new Student("Angel", "Motta", "angel@gmail.com");
+		studentDAO.save(s);
+		System.out.println("Student created with ID: " + s.getId() + " -> successfully saved in Database");
 	}
 }
